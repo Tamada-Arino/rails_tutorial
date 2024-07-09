689b3ffa0fd0fade01d5a2304bfed126991adb54
xaiBUh29wX <arino.tamada@luxiar.com>
2024-07-09 16:14:43 +0900

    routes.rbを編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   namespace :admin do
-    get 'users/new'
-    get 'users/edit'
-    get 'users/show'
-    get 'users/index'
+    resources :users only: %i[new edit show index]
   end
   root to: 'tasks#index'
   resources :tasks
