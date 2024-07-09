c40a964b2620613add4118f11e11f805373f6626
xaiBUh29wX <arino.tamada@luxiar.com>
2024-07-09 10:35:27 +0900

    コントローラーを修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,12 +9,12 @@ class TasksController < ApplicationController
   def create
     task = Task.new(task_params)
     task.save!
-    redirect_to tasks_url, notice: "タスク「#{task.name}を登録しました。」"
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
   end
 
   private
 
   def task_params
-    params.require(:task).permit(:name, :discription)
+    params.require(:task).permit(:name, :description)
   end
 end
